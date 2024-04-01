98b54d57a3bd2f10798515460d62c4a7e1509ded
Anand Vardhan <133073364+Nandostream11@users.noreply.github.com>
2024-04-01 22:43:30 +0530

    Update March_RoboDog.ino for marchpast

--- March_RoboDog.ino
+++ March_RoboDog.ino
@@ -48,75 +48,21 @@ int val(int deg){
 }
 
 void loop() {
-  // Move the servo from minimum to maximum position
+  // Actuate the servo motors for performing March past 
 
-
-  // int time=0;
-  // int inc1=val(5);
-  // int inc2=val(7.5);
-  // int curr1=90;
-  // int curr2=105;
-  // while(time<4){
-  //   curr1+=inc1;
-  //   curr2-=inc2;
-  //   pwm.setPWM(SERVO_4,0,curr1);
-  //   pwm.setPWM(SERVO_5,0,curr2);
-  //   delay(1000);
-  //   time++;
-  // }
-  //   time=0;
-  //   while(time<4){
-  //   curr1-=inc1;
-  //   curr2+=inc2;
-  //   pwm.setPWM(SERVO_4,0,curr1);
-  //   pwm.setPWM(SERVO_5,0,curr2);
-  //   delay(1000);
-  //   time++;
-  // }
-  // for (int p1 = val(110),p2=val(65),p3 = val(105),p4 = val(50); p1 >= val(90) && p2 <= val(105) && p3 >= val(85) && p4<= val(90); p1-=val(2.5),p2+=val(5),p3-=val(2.5),p4+=val(5)){
-  //   delay(1000);
-  //   pwm.setPWM(SERVO_4,0,p1);
-  //   pwm.setPWM(SERVO_5,0,p2);
-  //   pwm.setPWM(SERVO_7,0,p3);
-  //   pwm.setPWM(SERVO_8,0,p4);
-  // }
-  // for (int p1 = val(90),p2=val(105),p3 = val(85),p4 = val(90); p1 <= val(110) && p2 >= val(65) && p3 <= val(105) && p4>= val(50); p1+=val(2.5),p2-=val(5),p3+=val(2.5),p4-=val(5)){
-  //   delay(1000);
-  //   pwm.setPWM(SERVO_4,0,p1);//20deg
-  //   pwm.setPWM(SERVO_5,0,p2);//40deg
-  //   pwm.setPWM(SERVO_7,0,p3);
-  //   pwm.setPWM(SERVO_8,0,p4);
-  // }
-  
-  // delay(2000);
-    
-  // for (int p1 = val(70),p2=val(130),p3 = val(110),p4 = val(130); p1 <= val(90) && p2 >= val(90) && p3 >= val(90) && p4>= val(90); p1+=val(2.5),p2-=val(5),p3-=val(2.5),p4-=val(5)){
-  //   delay(1000);
-  //   pwm.setPWM(SERVO_1,0,p1);
-  //   pwm.setPWM(SERVO_2,0,p2);
-  //   pwm.setPWM(SERVO_10,0,p3);
-  //   pwm.setPWM(SERVO_11,0,p4);
-  // }
-  // for (int p1 = val(90),p2=val(90),p3 = val(90),p4 = val(90); p1 >= val(70) && p2 <= val(130) && p3 <= val(110) && p4<= val(130); p1-=val(2.5),p2+=val(5),p3+=val(2.5),p4+=val(5)){
-  //   delay(1000);
-  //   pwm.setPWM(SERVO_1,0,p1);
-  //   pwm.setPWM(SERVO_2,0,p2);
-  //   pwm.setPWM(SERVO_10,0,p3);
-  //   pwm.setPWM(SERVO_11,0,p4);
-  // }
   for (int p1 = val(105),p2=val(75),p3 = val(95),p4 = val(80); p1 >= val(90) && p2 <= val(105) && p3 >= val(85) && p4<= val(110); p1-=val(1),p2+=val(2),p3-=val(1),p4+=val(2)){
     delay(400);
     pwm.setPWM(SERVO_4,0,p1);
     pwm.setPWM(SERVO_5,0,p2);
-    // pwm.setPWM(SERVO_7,0,p3);
-    // pwm.setPWM(SERVO_8,0,p4);
+    pwm.setPWM(SERVO_7,0,p3);
+    pwm.setPWM(SERVO_8,0,p4);
   }
   for (int p1 = val(90),p2=val(105),p3 = val(85),p4 = val(110); p1 <= val(100) && p2 >= val(75) && p3 <= val(95) && p4>= val(80); p1+=val(1),p2-=val(2),p3+=val(1),p4-=val(2)){
     delay(400);
     pwm.setPWM(SERVO_4,0,p1);//20deg
     pwm.setPWM(SERVO_5,0,p2);//40deg
-    // pwm.setPWM(SERVO_7,0,p3);
-    // pwm.setPWM(SERVO_8,0,p4);
+    pwm.setPWM(SERVO_7,0,p3);
+    pwm.setPWM(SERVO_8,0,p4);
   } 
   delay(100);
     
@@ -124,35 +70,14 @@ void loop() {
     delay(400);
     pwm.setPWM(SERVO_1,0,p1);
     pwm.setPWM(SERVO_2,0,p2);
-    // pwm.setPWM(SERVO_10,0,p3);
-    // pwm.setPWM(SERVO_11,0,p4);
+    pwm.setPWM(SERVO_10,0,p3);
+    pwm.setPWM(SERVO_11,0,p4);
   }
   for (int p1 = val(90),p2=val(90),p3 = val(90),p4 = val(85); p1 >= val(80) && p2 <= val(120) && p3 <= val(100) && p4<= val(115); p1-=val(1),p2+=val(2),p3+=val(1),p4+=val(2)){
     delay(400);
     pwm.setPWM(SERVO_1,0,p1);
     pwm.setPWM(SERVO_2,0,p2);
-    // pwm.setPWM(SERVO_10,0,p3);
-    // pwm.setPWM(SERVO_11,0,p4);
+    pwm.setPWM(SERVO_10,0,p3);
+    pwm.setPWM(SERVO_11,0,p4);
   }
-  // for (int pulselen = val2; pulselen > val1; pulselen--){
-  //   pwm.setPWM(SERVO_1,0,pulselen);
-  //   delay(100);
-  // }
-  // Serial.println(pwm.getPWM(SERVO_1,true));
-  // for (int pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-  //   pwm.setPWM(SERVO_1, 0, pulselen);
-  //   pwm.setPWM(SERVO_2, 0, pulselen);
-  //   delay(10);
-  // }
-
-  
-  // Move the servo from maximum to minimum position
-  // for (int pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-  //   uint16_t onTime=pwm.getPWM(SERVO_1,true);
-  //   Serial.println(onTime);
-  //   pwm.setPWM(SERVO_1, 0, pulselen);
-  //   pwm.setPWM(SERVO_2, 0, pulselen);
-  //   delay(100);
-  // }
-
 }
